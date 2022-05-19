f4b29f3535ec1bee11b4f33a2408d19b846e096c
Santiago Chamorro <santychamorro29@gmail.com>
2022-05-19 15:52:18 -0300

    Agrega apariencia basica de Simon Dice
    Se construye el juego Simon Dice con la funcionalidad de presionar cada caja de color (con pseudoclase :active) para que el usuario sepa cuando y que esta presionando

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("Prueba");
+//const $turnoQuien = document.querySelector('#turno-quien');
+//$turnoQuien.value = 'Turno del jugador'
+const $cajaAzul = document.querySelector('.caja-azul');
+$cajaAzul.onclick = function funcionPrueba(){
+    console.log('Caja azul supremacy!');
+}
+
