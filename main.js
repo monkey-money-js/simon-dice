e46d1e8c344db8be88565efbd77c278e859a7d56
Santiago Chamorro <santychamorro29@gmail.com>
2022-05-23 09:19:58 -0300

    Cambio de sombras
    Se modificaron las sombras de la caja roja y verde

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ console.log("Prueba");
 //const $turnoQuien = document.querySelector('#turno-quien');
 //$turnoQuien.value = 'Turno del jugador'
 const $cajaAzul = document.querySelector('.caja-azul');
-$cajaAzul.onclick = function funcionPrueba(){
-    console.log('Caja azul supremacy!');
+function funcionPrueba(){
+    $cajaAzul.style.backgroundColor = 'blue';
 }
 
